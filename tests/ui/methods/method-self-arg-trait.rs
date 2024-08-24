27d118d782f1237b47358ad087c0d349f651cfc0
Obei Sideg <obei.sideg@gmail.com>
2024-08-24 06:49:09 +0300

    Update tests for hidden references to mutable static

--- tests/ui/methods/method-self-arg-trait.rs
+++ tests/ui/methods/method-self-arg-trait.rs
@@ -1,44 +1,62 @@
 //@ run-pass
 // Test method calls with self as an argument
 
+// FIXME(static_mut_refs): Do not allow `static_mut_refs` lint
+#![allow(static_mut_refs)]
+
 static mut COUNT: u64 = 1;
 
 #[derive(Copy, Clone)]
 struct Foo;
 
-trait Bar : Sized {
+trait Bar: Sized {
     fn foo1(&self);
     fn foo2(self);
     fn foo3(self: Box<Self>);
 
     fn bar1(&self) {
-        unsafe { COUNT *= 7; }
+        unsafe {
+            COUNT *= 7;
+        }
     }
     fn bar2(self) {
-        unsafe { COUNT *= 11; }
+        unsafe {
+            COUNT *= 11;
+        }
     }
     fn bar3(self: Box<Self>) {
-        unsafe { COUNT *= 13; }
+        unsafe {
+            COUNT *= 13;
+        }
     }
 }
 
 impl Bar for Foo {
     fn foo1(&self) {
-        unsafe { COUNT *= 2; }
+        unsafe {
+            COUNT *= 2;
+        }
     }
 
     fn foo2(self) {
-        unsafe { COUNT *= 3; }
+        unsafe {
+            COUNT *= 3;
+        }
     }
 
     fn foo3(self: Box<Foo>) {
-        unsafe { COUNT *= 5; }
+        unsafe {
+            COUNT *= 5;
+        }
     }
 }
 
 impl Foo {
     fn baz(self) {
-        unsafe { COUNT *= 17; }
+        unsafe {
+            COUNT *= 17;
+        }
+
         // Test internal call.
         Bar::foo1(&self);
         Bar::foo2(self);
@@ -63,5 +81,6 @@ fn main() {
 
     x.baz();
 
-    unsafe { assert_eq!(COUNT, 2*2*3*3*5*5*7*7*11*11*13*13*17); }
+    let count = unsafe { COUNT };
+    assert_eq!(count, 2 * 2 * 3 * 3 * 5 * 5 * 7 * 7 * 11 * 11 * 13 * 13 * 17);
 }
